8fe55227c7850a100af0273b60e8a66cf4d9e8a1
Oto Brglez <otobrglez@gmail.com>
2015-03-05 22:58:27 +0100

    Adding my first initial code

--- Climbing Trip/ViewController.swift
+++ Climbing Trip/ViewController.swift
@@ -10,6 +10,35 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+  
+  @IBOutlet weak var distanceTextField: NSTextField!
+  
+  @IBOutlet weak var unitComboBox: NSComboBox!
+  
+  @IBOutlet weak var outputTextField: NSTextField!
+  
+  func fe_days_needed(var distance: Int, unit:String = "km") -> Int {
+    var out_distance = Double(distance)
+    
+    if unit == "km" {
+      out_distance = out_distance * 0.621371
+    }
+    
+    var p:Double = Double(Int(out_distance) / 100)
+    
+    if p < 0.0 || p == 0.0 {
+      return 1
+    } else {
+      return Int(p)
+    }
+  }
+  
+  @IBAction func calculateButtonClick(sender: NSButton) {
+    var distance:Int = distanceTextField.integerValue
+    var output:String = String(format: "Days needed: %d", fe_days_needed(distance, unit: unitComboBox.stringValue))
+    outputTextField.stringValue = output
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
---
+++ README.md
@@ -0,0 +1,6 @@
+# Climbing Trip Calculator
+
+Climbing trip calculator calculates days needed for climbing trips, based on driving distanice.
+Calculator uses Dennis Hornings [floor equation (Climbing Days Per Mile)](http://www.climbing.com/blog/climbing-trip-worth-drive-math/) described in Climbing magazine.
+
+- Oto
